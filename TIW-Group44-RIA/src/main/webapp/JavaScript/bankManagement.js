e8970e6c93ddf879bcd5c3fa9efe06be2c4f81e3
Marco Baratto <marcobaratto2000@gmail.com>
2022-06-22 10:22:38 +0200

    fixed error showing contacts

--- TIW-Group44-RIA/src/main/webapp/JavaScript/bankManagement.js
+++ TIW-Group44-RIA/src/main/webapp/JavaScript/bankManagement.js
@@ -52,13 +52,11 @@
 			this.transferOkDiv.style.display = 'block';
 			this.addContactButton.style.display = 'block';
 		
-			if(addressBook !== undefined) {
-				var contactsAlreadyPresent = addressBook.has(transfer.idOwnerTo.toString())? addressBook.get(transfer.idOwnerTo.toString()): [];
+			if(addressBook !== undefined && addressBook.has(transfer.idOwnerTo.toString())) {
+				var contactsAlreadyPresent = addressBook.get(transfer.idOwnerTo.toString());
 				var array = Array.from(contactsAlreadyPresent);
-				if(array.includes(transfer.idBankAccountTo)){
+				if(array.includes(transfer.idBankAccountTo))
 					this.addContactButton.style.display = 'none';
-				}else
-					this.addContactButton.style.display = 'block';
 			}else
 				this.addContactButton.style.display = 'block';
 			
